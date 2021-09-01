8024ee90e0a107c3c130699eb31a0929ef93372a
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2021-09-01 10:19:20 +0200

    go-vim install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# install go-vim
+git clone https://github.com/fatih/vim-go.git "$HOME"/.vim/pack/plugins/start/vim-go
+
 me=$(dirname "${BASH_SOURCE[0]}")
 cp "${me}"/.vimrc "$HOME"/.vimrc
