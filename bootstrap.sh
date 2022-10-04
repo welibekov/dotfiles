9d5e063f04975e0e2309b362b0027a9baf9da65d
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2022-10-04 21:42:35 +0200

    Remove verbosity; getting pluings installation back

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x 
-
 # install go-vim
 plugins=$HOME/.vim/pack/plugins/start
 default=$HOME/.vim/pack/default/start
@@ -9,9 +7,6 @@ default=$HOME/.vim/pack/default/start
 [[ -d $plugins/vim-go ]] || git clone https://github.com/fatih/vim-go.git "$plugins"/vim-go
 [[ -d $default/gruvbox ]] || git clone https://github.com/morhetz/gruvbox.git "$default"/gruvbox
 
-echo bash_source="${BASH_SOURCE}"
-echo bash_source_0="${BASH_SOURCES[0]}"
-
 me=$(dirname "${BASH_SOURCE[0]}")
 cp "${me}"/.vimrc "$HOME"/.vimrc
-#vim -c ":GoInstallBinaries" -c ":x"
+vim -c ":GoInstallBinaries" -c ":x"
