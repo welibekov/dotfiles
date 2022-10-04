1dd77d8660e0b9c4128e78d0fce01c0d5134d9b0
Eldar Valibayov <ext.Eldar.Valibayov@exasol.com>
2022-10-04 21:19:31 +0200

    Add some output info

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,9 @@ default=$HOME/.vim/pack/default/start
 [[ -d $plugins/vim-go ]] || git clone https://github.com/fatih/vim-go.git "$plugins"/vim-go
 [[ -d $default/gruvbox ]] || git clone https://github.com/morhetz/gruvbox.git "$default"/gruvbox
 
+echo bash_source="${BASH_SOURCE}"
+echo bash_source_0="${BASH_SOURCES[0]}"
+
 me=$(dirname "${BASH_SOURCE[0]}")
 cp "${me}"/.vimrc "$HOME"/.vimrc
 #vim -c ":GoInstallBinaries" -c ":x"
